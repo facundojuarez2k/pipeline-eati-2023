9ccdb835d30bb78c4dba93f42a157cd162152965
Facundo Juárez <facundojuarezf@outlook.com>
2023-03-02 15:38:48 -0300

    Agrega flake8

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Program's main module
+'''
+
 import os
 
 BUILD_DIR = os.path.join(os.getcwd(), "build")
@@ -7,7 +11,8 @@ HTML_CONTENT = '''<!DOCTYPE html >
 <html lang = "es" >
    <head >
         <meta charset = "UTF-8" >
-        <meta name = "viewport" content = "width=device-width, initial-scale=1.0" >
+        <meta name = "viewport" content = \
+"width=device-width, initial-scale=1.0" >
         <meta http-equiv = "X-UA-Compatible" content = "ie=edge" >
         <title > HTML 5 Boilerplate < /title >
     </head >
@@ -18,10 +23,12 @@ HTML_CONTENT = '''<!DOCTYPE html >
 
 
 def main():
+    '''
+    Creates the index.html file under build directory
+    '''
     # Create build directory
     if not os.path.exists(BUILD_DIR):
         os.mkdir(BUILD_DIR)
     # Create HTML file
-    with open(FILE_PATH, "w") as fp:
-        fp.write(HTML_CONTENT)
-        pass
+    with open(FILE_PATH, "w", encoding="UTF-8") as file_pointer:
+        file_pointer.write(HTML_CONTENT)
